26c4b8fd75270bccf2fc29957f383c1d4a060be2
Uri3lArchangel <solomonhemigadon@gmail.com>
2023-04-03 19:45:41 -0400

    mod11

--- api/index.js
+++ api/index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const routes = require('./router/server')
+const routes = require('../router/server')
 const path=require("path")
 
 app.use(express.static(path.join(__dirname,'client', 'js')));
@@ -9,9 +9,5 @@ app.use(express.static(path.join(__dirname,'client', 'js')));
 app.listen(3001, (err, res) => console.log('server started on port 3001'));
 
 app.use(routes)
-app.use((req,res)=>{
-    res.status(200)
-    res.sendFile(path.join(__dirname,'client','index.html'))
-})
 
 
