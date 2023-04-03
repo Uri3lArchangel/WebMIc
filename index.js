11ae641cd52a77cfc74e06b7033690e453777aff
Uri3lArchangel <solomonhemigadon@gmail.com>
2023-04-03 18:40:57 -0400

    mod3

--- index.js
+++ index.js
@@ -4,5 +4,9 @@ const server = require('./api/server')
 
 app.use(express.static('./api/public'));
 app.use('/',server)
+app.use('/upload',server)
+app.use('/download',server)
+
+
 
 app.listen(3001, (err, res) => console.log('server started on port 3001'));
