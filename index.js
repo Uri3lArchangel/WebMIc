09808c32eab0bc86bad1be037fcd5c22f94b3e92
Uri3lArchangel <solomonhemigadon@gmail.com>
2023-04-03 19:32:55 -0400

    mod10

--- index.js
+++ index.js
@@ -9,5 +9,9 @@ app.use(express.static(path.join(__dirname,'client', 'js')));
 app.listen(3001, (err, res) => console.log('server started on port 3001'));
 
 app.use(routes)
+app.use((req,res)=>{
+    res.status(200)
+    res.sendFile(path.join(__dirname,'client','index.html'))
+})
 
 
